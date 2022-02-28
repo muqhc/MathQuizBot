096e5af9187a48c25df8ea318dbb8260ea969c79
muqhc <muqhc07@gmail.com>
2022-02-28 23:06:54 +0900

    run bot in sub thread

--- MathQuizBot/Program.fs
+++ MathQuizBot/Program.fs
@@ -4,40 +4,42 @@ open Discord
 open Discord.WebSocket
 open Discord.Commands
 open System
+open System.Diagnostics
 open System.Threading
+open System.Threading.Tasks
 
 open MathQuizBot.Commands
 
 let logging (log: LogMessage) = log.ToString() |> printfn "%s"
 
-let run (args: string[]) = 
-    task {
-        printfn "Bot Booting....."
+let run (token: string) (args: string[]) (someObj: obj) = 
+    printfn "Bot Booting....."
+    
+    let client = new DiscordSocketClient()
+    let commandServ = new CommandService()
+    
+    client.add_Log      (fun log -> task { logging log })
+    commandServ.add_Log (fun log -> task { logging log })
+    
+    client.LoginAsync (TokenType.Bot, token) |> Async.AwaitTask |> ignore
+    client.StartAsync () |> Async.AwaitTask |> ignore
+    
+    client.add_Ready <| Command.publishCmdGlobal client [|
+        PingCommand ()
+        FoodCommand client
+        MathQuizCommand client
+    |]
+    Thread.Sleep(-1)
 
-        let client = new DiscordSocketClient()
-        let commandServ = new CommandService()
-
-        client.add_Log      (fun log -> task { logging log })
-        commandServ.add_Log (fun log -> task { logging log })
-
-        let token = match Environment.GetEnvironmentVariable "BOT_TOKEN" with
+[<EntryPoint>]
+let main (args: string[]): int =
+    let token = match Environment.GetEnvironmentVariable "BOT_TOKEN" with
                     | null ->
                         printf "Write Bot Token : "
                         Console.ReadLine()
                     | it -> it
-        client.LoginAsync (TokenType.Bot, token) |> Async.AwaitTask |> ignore
-        client.StartAsync () |> Async.AwaitTask |> ignore
-
-        client.add_Ready <| Command.publishCmdGlobal client [|
-            PingCommand()
-            FoodCommand(client)
-            MathQuizCommand(client)
-        |] 
-
-        Thread.Sleep(-1)
-    }
-
-[<EntryPoint>]
-let main (args: string[]): int =
-    run args |> ignore
+    let botFun = run token args
+    let botThread = Thread(ParameterizedThreadStart botFun)
+    botThread.Start()
+    printfn "Main Thread End."
     0
